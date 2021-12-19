c675a482159c35e4a20b7956bcaa5de8e75d87cc
iman-aazibou <imanaazibou@hotmail.com>
2021-12-19 23:10:59 +0100

    adding item in cart

--- src/Components/Meals/MealItem/MealItem.js
+++ src/Components/Meals/MealItem/MealItem.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, {useContext} from "react";
+
+
 import MealItemForm from "./MealItemForm";
 import classes from "./MealItem.module.css";
+import CartContext from "../../../store/cart-context";
 
 const MealItem = (props) => {
+
+  const cartCtx = useContext(CartContext);
+
   //to render two decimals 
   const price = `${props.price.toFixed(2)}€`;
+
+  const addToCartHandler = amount => {
+
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price
+    })
+
+
+  }
   return (
     <li className={classes.meal}>
       <div>
@@ -17,7 +35,7 @@ const MealItem = (props) => {
         </div>
       </div>
       <div>
-        <MealItemForm id={props.id}/>
+        <MealItemForm onAddToCart={addToCartHandler} id={props.id}/>
           
       </div>
     </li>
