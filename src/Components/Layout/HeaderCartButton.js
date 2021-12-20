4cf613069606ee29aa20f816a24095f4191c22da
iman-aazibou <imanaazibou@hotmail.com>
2021-12-20 21:28:17 +0100

    highlate button everytime an item is added

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,19 +1,38 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlated, setBtnIsHighlated] = useState(false);
+
   const cartCtx = useContext(CartContext);
- 
-  const numberOfItemsInCart = cartCtx.items.reduce((currentNum, item) => {
+  const { items } = cartCtx;
+
+  const numberOfItemsInCart = items.reduce((currentNum, item) => {
     return currentNum + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${btnIsHighlated ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+
+    setBtnIsHighlated(true);
+    const timer = setTimeout(() => {
+      setBtnIsHighlated(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
 
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
